9381b37771aa9b683cada631ae135a2855696587
Igor-Lima07 <igorlego86@gmail.com>
2026-01-01 22:58:52 -0300

    Arruma alguns problemas de css e adiciona botoes simples de crud

--- script.js
+++ script.js
@@ -10,17 +10,25 @@ atualizarLista = function(){
     tasks.forEach(task => {
         tarefa = document.createElement("li");
         checkbox = document.createElement("input")
+        delBtn = document.createElement("button")
+        editBtn = document.createElement("button")
         checkbox.type = "checkbox"
+        editBtn.classList.add("editBtn")
+        delBtn.classList.add("delBtn")
         checkbox.classList.add("checkbox")
+        subContainer = document.createElement("subContainer")
+        subContainer.classList.add("subContainer")
         container = document.createElement("container")
         container.classList.add("container")
         tarefaText = document.createElement("span")
 
         tarefaText.textContent = task
 
-
     container.appendChild(tarefaText)
-    container.appendChild(checkbox)
+    container.appendChild(subContainer)
+    subContainer.appendChild(checkbox)
+    subContainer.appendChild(editBtn)
+    subContainer.appendChild(delBtn)
 
     tarefa.appendChild(container)
     list.appendChild(tarefa);
